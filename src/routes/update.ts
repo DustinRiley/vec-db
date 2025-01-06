3c94dc771e03493b7d4430b8a07a75c4af6dafec
DustinRiley <dustin.riley@me.com>
2025-01-06 13:01:23 -0800

    new endpoints

--- src/routes/update.ts
+++ src/routes/update.ts
@@ -1,32 +1,58 @@
 import { Router } from "express";
 import multer from "multer";
-import { storeFileEmbedding } from "../services/database";
+import { storeFileEmbedding, updateFileEmbedding } from "../services/database";
 import { checkAuth } from "../services/checkAuth";
 import { generateEmbedding } from "../services/embeddings";
 import { extractTextFromPDF } from "../services/document_parsing/pdf";
 import { MAX_CHUNKS } from "../constants";
 
-export const uploadRouter = Router();
+export const updateRouter = Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const singleFileUpload = upload.single("file");
 
-uploadRouter.use(checkAuth);
+updateRouter.use(checkAuth);
 
-uploadRouter.post("/", singleFileUpload, async (req, res) => {
+updateRouter.post("/", singleFileUpload, async (req, res) => {
+  
   try {
+    const {file, body} = req;
+    const { fileId, content, metadata, id} = body;
+
+    const hasFileIdentifier = !!fileId || !!id;
+    const hasContent = !!content || !!file;
+    if(!hasFileIdentifier || !hasContent) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
     if (!req.file) {
       return res.status(400).json({ error: "No file provided." });
     }
 
-    const chunks = await extractTextFromPDF(req.file.buffer);
+    if(fileId && !file) {
+      return res.status(400).json({ error: "No file provided." });
+    }
+
+    if(id && !content) {
+      return res.status(400).json({ error: "No content provided." });
+    }
+    if(id && fileId) {
+      return res.status(400).json({ error: "Cannot provide both id and fileId" });
+    } 
+    
+    let chunks = [];
+    if(file){
+      chunks = await extractTextFromPDF(req.file.buffer);
+    } else {
+      chunks = [content];
+    }
+
+
     if (chunks.length === 0 || chunks.length > MAX_CHUNKS) {
       return res.status(400).json({ error: "Could not extract text from file!!" });
     }
 
-    // todo do better file id generation
-    const fileId = req.file.originalname + "-" + Math.random().toString(36).substring(7);
 
     for (const text of chunks) {
       console.log("Chunk:", text);
@@ -36,7 +62,7 @@ uploadRouter.post("/", singleFileUpload, async (req, res) => {
 
       const embedding = await generateEmbedding(text);
 
-      await storeFileEmbedding({ fileId, content: text, vector: embedding });
+      await updateFileEmbedding({ fileId, content: text, vector: embedding, id });
     }
     return res.status(200).json({ message: "File uploaded successfully" });
   } catch (err: any) {
@@ -45,5 +71,3 @@ uploadRouter.post("/", singleFileUpload, async (req, res) => {
   }
 });
 
-// TODO: store in an S3 bucket?
-// idk if we wanna do that for security reasons though
